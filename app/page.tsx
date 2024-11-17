4adc573bd98e63e749a70816029eae0362486d14
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-11-17 14:29:30 +0100

    adjust background colors

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Test</h1>
+      <header>
+        <h1>header</h1>
+      </header>
+      <section id={styles.focus}>focus cards</section>
+      <section id={styles.projets}>Projets</section>
     </div>
   );
 }
