71c9b1a49c79e1e68651acd6e8fe77d6433c872d
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-11-18 08:42:50 +0100

    Set active nav links

--- app/ui/topbar/nav/Nav.tsx
+++ app/ui/topbar/nav/Nav.tsx
@@ -1,20 +1,30 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import styles from "./nav.module.css";
+import { useEffect } from "react";
 
 function Nav() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    console.log(pathname);
+  });
+
   return (
     <nav id={styles.navbar}>
       <ul>
-        <li className={styles.active}>
+        <li className={`${pathname === "/" && styles.active}`}>
           <Link href="/">Accueil</Link>
         </li>
-        <li>
+        <li className={`${pathname === "/projets" && styles.active}`}>
           <Link href="/projets">Projets</Link>
         </li>
-        <li>
+        <li className={`${pathname === "/a-propos" && styles.active}`}>
           <Link href="/a-propos">A propos</Link>
         </li>
-        <li>
+        <li className={`${pathname === "/contact" && styles.active}`}>
           <Link href="/contact">Contact</Link>
         </li>
       </ul>
