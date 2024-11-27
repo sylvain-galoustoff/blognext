f1a1c7cbab392d21fe2477ab3504336cfa5155e2
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-11-27 16:15:43 +0100

    Footer set up

--- app/ui/footer/Footer.tsx
+++ app/ui/footer/Footer.tsx
@@ -1,7 +1,22 @@
+import Separator from "../separator/Separator";
 import styles from "./footer.module.css";
+import { IoMail } from "react-icons/io5";
 
 function Footer() {
-  return <footer id={styles.footer}>FOOTER</footer>;
+  return (
+    <footer id={styles.footer}>
+      <p id={styles.name}>Sylvain Galoustoff</p>
+      <Separator background="text" />
+      <div id={styles.jobs}>
+        <p>Développeur front-end / React</p>
+        <p>UI Designer</p>
+      </div>
+      <p id={styles.contact}>
+        <IoMail />
+        contact@sylvain-galoustoff.dev
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
